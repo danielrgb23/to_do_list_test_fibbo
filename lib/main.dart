2529eebd281a5ed5bd0d680d58aa78bf60a413a3
Daniel Jasmelino <danielrgb2@gmail.com>
2024-11-27 18:57:10 -0300

    feat: integrando provider e firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,47 @@
+import 'screens/home_screen.dart';
+import 'providers/task_provider.dart';
+import 'screens/add_task_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:to_do_list/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:to_do_list/services/auth_service.dart';
 
-void main() {
-  runApp(const MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider(create: (_) => AuthService()),
+        ChangeNotifierProvider(create: (_) => TaskProvider()..loadTasks()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'To_do_list',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const Scaffold());
+      title: 'To do List',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      darkTheme: ThemeData.dark(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/add': (context) => AddTaskScreen(),
+      },
+    );
   }
 }
