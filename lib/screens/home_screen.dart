82e6eb20c35bd34c7578aaa050fb8dfe1a6503ff
Daniel Jasmelino <danielrgb2@gmail.com>
2024-11-29 00:33:20 -0300

    feat: ajuste nas rotas, nomes, utilização de consumer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,8 +10,6 @@ import 'package:to_do_list/screens/auth_screen.dart';
 import 'package:to_do_list/services/auth_service.dart';
 import 'package:to_do_list/screens/profile_screen.dart';
 
-
-
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -29,210 +27,214 @@ class _HomeScreenState extends State<HomeScreen>
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
     auth != null
-        ? context.read<TaskProvider>().backupTasks(auth!.uid)
+        ? {context.read<TaskProvider>().backupVisibleTasksToFirebase(auth!.uid), context.read<TaskProvider>().setGuestMode(false)}
         : log('Não esta logado');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Gerenciador de Tarefas'),
-          actions: [
-            PopupMenuButton<String>(
-              onSelected: (value) {
-                setState(() {
-                  selectedPriority = value == 'Todas' ? null : value;
-                });
-              },
-              child: const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  'Filtrar',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                ),
+      appBar: AppBar(
+        title: const Text('Gerenciador de Tarefas'),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              setState(() {
+                selectedPriority = value == 'Todas' ? null : value;
+              });
+            },
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Filtrar',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
               ),
-              itemBuilder: (context) => [
-                const PopupMenuItem(
-                  value: 'Todas',
-                  child: Text('Todas'),
-                ),
-                const PopupMenuItem(
-                  value: 'Alta',
-                  child: Text('Alta'),
-                ),
-                const PopupMenuItem(
-                  value: 'Média',
-                  child: Text('Média'),
-                ),
-                const PopupMenuItem(
-                  value: 'Baixa',
-                  child: Text('Baixa'),
-                ),
-              ],
             ),
-          ],
-          bottom: TabBar(
-            controller: _tabController,
-            tabs: const [
-              Tab(text: 'Pendentes'),
-              Tab(text: 'Concluídas'),
+            itemBuilder: (context) => [
+              const PopupMenuItem(
+                value: 'Todas',
+                child: Text('Todas'),
+              ),
+              const PopupMenuItem(
+                value: 'Alta',
+                child: Text('Alta'),
+              ),
+              const PopupMenuItem(
+                value: 'Media',
+                child: Text('Média'),
+              ),
+              const PopupMenuItem(
+                value: 'Baixa',
+                child: Text('Baixa'),
+              ),
             ],
           ),
-        ),
-        body: TabBarView(
+        ],
+        bottom: TabBar(
           controller: _tabController,
-          children: [
-            _buildTaskList(context, isCompleted: false),
-            _buildTaskList(context, isCompleted: true),
+          tabs: const [
+            Tab(text: 'Pendentes'),
+            Tab(text: 'Concluídas'),
           ],
         ),
-        floatingActionButton: ExpandableFab(
-          distance: 112,
-          children: [
-            ActionButton(
-              onPressed: () {
-                setState(() {
-                  isGroupedByPriority = !isGroupedByPriority;
-                });
-              },
-              icon: const Icon(Icons.grid_on_outlined),
-            ),
-            ActionButton(
-              onPressed: () {
-                auth != null
-        ? Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) =>  ProfileScreen(user: auth!,)),
-                )
-        : Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const AuthScreen()),
-                );;
-                
-              },
-              icon: const Icon(Icons.person),
-            ),
-            ActionButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const AddTaskScreen()),
-                );
-              },
-              icon: const Icon(Icons.add),
-            ),
-          ],
-        ));
-
-    //   FloatingActionButton.extended(
-    //     onPressed: () {
-    //       Navigator.push(
-    //         context,
-    //         MaterialPageRoute(builder: (_) => const AddTaskScreen()),
-    //       );
-    //     },
-    //     icon:  Icon(Icons.add),
-    //     label: Text('Mais'),
-    //   ),
-    // );
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          _buildTaskList(context, isCompleted: false),
+          _buildTaskList(context, isCompleted: true),
+        ],
+      ),
+      floatingActionButton: ExpandableFab(
+        distance: 112,
+        children: [
+          // ActionButton(
+          //   onPressed: () async {
+          //     await Provider.of<TaskProvider>(context, listen: false).handleLogout();
+          //   },
+          //   icon: const Icon(Icons.tap_and_play),
+          // ),
+          ActionButton(
+            onPressed: () {
+              setState(() {
+                isGroupedByPriority = !isGroupedByPriority;
+              });
+            },
+            icon: const Icon(Icons.grid_on_outlined),
+          ),
+          ActionButton(
+            onPressed: () {
+              auth != null
+                  ? Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => ProfileScreen(
+                                user: auth!,
+                              )),
+                    )
+                  : Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => const AuthScreen()),
+                    );
+              ;
+            },
+            icon: const Icon(Icons.person),
+          ),
+          ActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const AddTaskScreen()),
+              );
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildTaskList(BuildContext context, {required bool isCompleted}) {
-    final tasks = isCompleted
-        ? context.watch<TaskProvider>().completedTasks
-        : context.watch<TaskProvider>().pendingTasks;
-    final filteredTasks = selectedPriority == null
-        ? tasks
-        : tasks.where((task) => task.priority == selectedPriority).toList();
+    return Consumer<TaskProvider>(
+      builder: (context, taskProvider, _) {
+        final tasks = isCompleted
+            ? context.watch<TaskProvider>().completedTasks
+            : context.watch<TaskProvider>().pendingTasks;
+        final filteredTasks = selectedPriority == null
+            ? tasks
+            : tasks.where((task) => task.priority == selectedPriority).toList();
 
-    if (filteredTasks.isEmpty) {
-      return Center(
-        child: Text(
-          isCompleted
-              ? 'Nenhuma tarefa concluída.'
-              : 'Nenhuma tarefa pendente.',
-        ),
-      );
-    }
+            
 
-    if (isGroupedByPriority) {
-      final Map<String, List<Task>> groupedTasks = {
-        'Alta': [],
-        'Média': [],
-        'Baixa': [],
-      };
-
-      for (var task in filteredTasks) {
-        groupedTasks[task.priority]?.add(task);
-      }
-
-      return ListView(
-        children: [
-          if (groupedTasks['Alta']!.isNotEmpty) ...[
-            _buildPrioritySection('Alta', groupedTasks['Alta']!),
-          ],
-          if (groupedTasks['Média']!.isNotEmpty) ...[
-            _buildPrioritySection('Média', groupedTasks['Média']!),
-          ],
-          if (groupedTasks['Baixa']!.isNotEmpty) ...[
-            _buildPrioritySection('Baixa', groupedTasks['Baixa']!),
-          ],
-        ],
-      );
-    } else {
-      return ListView.builder(
-        itemCount: filteredTasks.length,
-        itemBuilder: (context, index) {
-          final task = filteredTasks[index];
-          return Card(
-            elevation: 2,
-            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            child: ListTile(
-              title: Text(task.title),
-              leading: Checkbox(
-                value: task.isCompleted,
-                onChanged: (value) {
-                  context.read<TaskProvider>().updateTask(
-                        Task(
-                          id: task.id,
-                          title: task.title,
-                          description: task.description,
-                          priority: task.priority,
-                          isCompleted: value!,
-                        ),
-                      );
-                },
-              ),
-              subtitle: Text(
-                'Prioridade: ${task.priority}',
-                style: TextStyle(
-                  color: task.priority == 'Alta'
-                      ? Colors.red
-                      : task.priority == 'Média'
-                          ? Colors.orange
-                          : Colors.green,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              trailing: IconButton(
-                  onPressed: () {
-                    context.read<TaskProvider>().deleteTask(task.id!);
-                  },
-                  icon: const Icon(Icons.delete_outline)),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => AddTaskScreen(task: task),
-                  ),
-                );
-              },
+        if (filteredTasks.isEmpty) {
+          return Center(
+            child: Text(
+              isCompleted
+                  ? 'Nenhuma tarefa concluída.'
+                  : 'Nenhuma tarefa pendente.',
             ),
           );
-        },
-      );
-    }
+        }
+        if (isGroupedByPriority) {
+          final Map<String, List<Task>> groupedTasks = {
+            'Alta': [],
+            'Media': [],
+            'Baixa': [],
+          };
+
+          for (var task in filteredTasks) {
+            groupedTasks[task.priority]?.add(task);
+          }
+
+          return ListView(
+            children: [
+              if (groupedTasks['Alta']!.isNotEmpty) ...[
+                _buildPrioritySection('Alta', groupedTasks['Alta']!),
+              ],
+              if (groupedTasks['Media']!.isNotEmpty) ...[
+                _buildPrioritySection('Media', groupedTasks['Media']!),
+              ],
+              if (groupedTasks['Baixa']!.isNotEmpty) ...[
+                _buildPrioritySection('Baixa', groupedTasks['Baixa']!),
+              ],
+            ],
+          );
+        } else {
+          return ListView.builder(
+            itemCount: filteredTasks.length,
+            itemBuilder: (context, index) {
+              log('${filteredTasks[index].userId}\n${filteredTasks[index].id}');
+              final task = filteredTasks[index];
+              return Card(
+                elevation: 2,
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: ListTile(
+                  title: Text(task.title),
+                  leading: Checkbox(
+                    value: task.isCompleted,
+                    onChanged: (value) {
+                      context.read<TaskProvider>().updateTask(
+                            Task(
+                                id: task.id,
+                                title: task.title,
+                                description: task.description,
+                                priority: task.priority,
+                                isCompleted: value!,
+                                isView: true),
+                          );
+                    },
+                  ),
+                  subtitle: Text(
+                    'Prioridade: ${task.priority}',
+                    style: TextStyle(
+                      color: task.priority == 'Alta'
+                          ? Colors.red
+                          : task.priority == 'Media'
+                              ? Colors.orange
+                              : Colors.green,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  trailing: IconButton(
+                      onPressed: () {
+                        context.read<TaskProvider>().deleteTask(task.id!);
+                      },
+                      icon: const Icon(Icons.delete_outline)),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => AddTaskScreen(task: task),
+                      ),
+                    );
+                  },
+                ),
+              );
+            },
+          );
+        }
+      },
+    );
   }
 
   Widget _buildPrioritySection(String priority, List<Task> tasks) {
@@ -248,7 +250,7 @@ class _HomeScreenState extends State<HomeScreen>
               fontSize: 18,
               color: priority == 'Alta'
                   ? Colors.red
-                  : priority == 'Média'
+                  : priority == 'Media'
                       ? Colors.orange
                       : Colors.green,
             ),
@@ -266,12 +268,12 @@ class _HomeScreenState extends State<HomeScreen>
                       onChanged: (value) {
                         context.read<TaskProvider>().updateTask(
                               Task(
-                                id: task.id,
-                                title: task.title,
-                                description: task.description,
-                                priority: task.priority,
-                                isCompleted: value!,
-                              ),
+                                  id: task.id,
+                                  title: task.title,
+                                  description: task.description,
+                                  priority: task.priority,
+                                  isCompleted: value!,
+                                  isView: true),
                             );
                       },
                     ),
@@ -280,7 +282,7 @@ class _HomeScreenState extends State<HomeScreen>
                       style: TextStyle(
                         color: task.priority == 'Alta'
                             ? Colors.red
-                            : task.priority == 'Média'
+                            : task.priority == 'Media'
                                 ? Colors.orange
                                 : Colors.green,
                         fontWeight: FontWeight.bold,
